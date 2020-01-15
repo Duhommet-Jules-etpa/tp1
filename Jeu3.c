baf864eba169d3d4445d933515fb1dae88fd719a
Duhommet-Jules-etpa <59608473+Duhommet-Jules-etpa@users.noreply.github.com>
2020-01-15 16:39:43 +0100

    Update Jeu3.c

--- Jeu3.c
+++ Jeu3.c
@@ -1,24 +1,33 @@
+
 #include <stdio.h>
 #include <string.h>
 int main() {
   int vie_j = 35;
-  int vie_m = 35;
+  int vie_m = 25;
   int attaque=1;
-  int dead=0;
   int contreattaque=1;
   vie_j=35;
-while(vie_j>0){
-	if(vie_j!=0){
+    while((vie_j>0) && (vie_m>0)){
 		printf("Point de vie du joueur = %d \n",vie_j);
 		printf("Vous attaquez le monstre et il perd 1 point de vie \n");
-		vie_j=vie_j-attaque;
+		vie_m=vie_m-attaque;
 	
-}
-vie_m=vie_m-contreattaque;
-printf("La contre attaque du monstre vous enleve 1 point de vie \n",vie_m);
-printf("Point de vie du monstre %d \n",vie_m);
+        vie_j=vie_j-contreattaque;
+        printf("La contre attaque du monstre vous enleve 1 point de vie \n");
+        printf("Point de vie du monstre %d \n",vie_m);
 
 	}
-	printf("Point de vie du monstre =%d \n", dead);
+	
+	printf("Point de vie du joueur =%d \n", vie_j);
+
+	
+	    if (vie_j <= 0){
+    printf("Le monstre a gagne");
+
+    }
+
+    else if (vie_m <= 0){
+        printf("Le joueur a gagne");
+    }
 	return 0;
 }
